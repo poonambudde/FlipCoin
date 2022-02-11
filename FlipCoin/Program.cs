8b6ab063b35f0a2d2419a394fa50fe16e759f1ed
poonambudde <buddepoonam123@gmail.com>
2022-02-12 00:13:23 +0530

    ADDED: Displays the winner Heads or Tails

---
+++ FlipCoin/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int HEAD = 1;
+            const int TAIL = 0;
+
+            Random random = new Random();
+            int flipCheck = random.Next(0, 2);
+            if (flipCheck == HEAD)
+            {
+                Console.WriteLine("HEAD wins");
+            }
+            else
+            {
+                Console.WriteLine("TAIL wins");
+
+            }
+
+        }
+    }
+}
